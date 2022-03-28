ab27908f302773ef947365fceab0100ba872aaa8
Krrish105 <jainkarishma76@gmail.com>
2022-03-28 20:56:17 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./style.css";
 import Home from "./Components/Home";
 import Project from "./Components/Project";
 import Contact from "./Components/Contact";
-// import ScrollToTop from "./Components/ScrollTop";
+import ScrollToTop from "./Components/ScrollTop";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -19,7 +19,7 @@ export default function App() {
         <hr />
 
         {/* Main */}
-        {/* <ScrollToTop> */}
+        <ScrollToTop>
           <main>
             <Routes>
               <Route exact path='/karishmagarg/' element={ <Home /> }></Route>
@@ -27,7 +27,7 @@ export default function App() {
               <Route exact path='/karishmagarg/contact' element={ <Contact /> }></Route>
             </Routes>
           </main>
-        {/* </ScrollToTop> */}
+        </ScrollToTop>
         <hr />
 
         {/* Footer */}
